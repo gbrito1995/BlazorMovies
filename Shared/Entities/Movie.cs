8ac526b416c4990ea3cfe4e330d387259231686c
Gabriel Brito <gabrieldsb95@gmail.com>
2022-08-16 22:26:14 -0300

    commit

---
+++ Shared/Entities/Movie.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorMovies.Shared.Entities
+{
+    public class Movie
+    {
+        public DateTime RealeaseDate{get; set;}
+        public string? Title{get; set;}
+    }
+}
